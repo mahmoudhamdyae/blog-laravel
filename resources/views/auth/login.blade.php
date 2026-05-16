54526c1ee99c4fb38c796e5af99336aaf25a15d4
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2026-05-16 21:15:11 +0300

    end api

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,6 +53,11 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
+
+                             <button type="submit" class="btn btn-dark">
+                                    Login with Github
+                                </button>
+
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
